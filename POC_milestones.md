4ccfdfc6dd953173b46b31abe796c63677a25934
Francisco Cuenya Folgar <francisco.cuenya@gmail.com>
2024-11-18 18:19:13 +0000

    upgrades server POC with album artwork retrieval from the media files

--- POC_milestones.md
+++ POC_milestones.md
@@ -8,7 +8,7 @@
 
 - [ ] More comprehensive/"stronger" data types and DB schema
   - [x] Setup Prisma for SQLite
-  - [ ] Load data
+  - [x] Load data
   - [ ] Find the most convenient format to store the media location
   - [ ] Move SQLite file to "data" folder
   - [ ] Document DB schema
@@ -16,3 +16,8 @@
 - [ ] Sharing types between client and server
 
 - [ ] PWA
+
+Learn more about
+
+- TS mode resolution
+- Monorepos (or other ways to share code between client and server)
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,21 +1,40 @@
 import express from 'express';
+import cors from 'cors';
 import prisma from './db';
+import path from 'path'
+import fs from 'fs';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors({
+    origin: 'http://localhost:5173' // Your Vite dev server
+}));
+
 app.use(express.json());
 
 // Example route using Prisma
-app.get('/api/tracks', async (req, res) => {
-    const tracks = await prisma.track.findMany({
+app.get('/api/albums', async (req, res) => {
+    const tracks = await prisma.album.findMany({
         include: {
-            artist: true,
-            album: true
-        }
+            tracks: true,
+        },
+        orderBy: { "id": "desc" }
+        // take: 500
     });
     res.json(tracks);
 });
+
+app.get('/artwork/:uuid', (req, res) => {
+    const artworkDir = path.join(__dirname, '../data/artwork');
+    const files = fs.readdirSync(artworkDir);
+    const artworkFile = files.find(file => file.startsWith(`${req.params.uuid}-artwork`));
+    if (artworkFile) {
+        res.sendFile(path.join(artworkDir, artworkFile))
+    } else {
+        res.status(404).send('Artwork not found')
+    }
+})
 
 // Ensure Prisma disconnects on app termination
 process.on('beforeExit', async () => {
